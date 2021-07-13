a27bf1b0dc5ff983078097ac7ea0a19e3f59b2b7
ColdFier <kodim1892@gmail.com>
2021-07-13 23:34:15 +0300

    Demo button was added

--- app/src/main/java/com/coldfier/peanuttest/signinfragment/SignInFragment.kt
+++ app/src/main/java/com/coldfier/peanuttest/signinfragment/SignInFragment.kt
@@ -1,21 +1,16 @@
 package com.coldfier.peanuttest.signinfragment
 
-import android.app.Activity
 import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.coldfier.peanuttest.databinding.SignInFragmentBinding
-import com.coldfier.peanuttest.repository.UserData
-import java.lang.NumberFormatException
-import kotlin.math.log
 
 class SignInFragment : Fragment() {
 
@@ -39,7 +34,7 @@ class SignInFragment : Fragment() {
                 if (loginEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
                     signInProgressBar.visibility = View.VISIBLE
                     signInButton.isClickable = false
-                    var login: Int = try {
+                    val login: Int = try {
                         loginEditText.text.toString().toInt()
                     } catch (e: NumberFormatException) {
                         0
@@ -48,11 +43,6 @@ class SignInFragment : Fragment() {
                             login,
                             passwordEditText.text.toString(),
                             requireContext()
-                    )
-                    viewModel.signIn(
-                        login,
-                        passwordEditText.text.toString(),
-                        requireContext()
                     )
                 } else {
                     Toast.makeText(
@@ -64,12 +54,19 @@ class SignInFragment : Fragment() {
             }
         }
 
+        binding.demoButton.setOnClickListener {
+            binding.signInProgressBar.visibility = View.VISIBLE
+            binding.demoButton.isClickable = false
+            viewModel.signIn(20234561, "ladevi31", requireContext())
+        }
+
         binding.loginEditText.setOnFocusChangeListener { v, hasFocus -> if (!hasFocus) hideKeyboard(v) }
         binding.passwordEditText.setOnFocusChangeListener { v, hasFocus -> if (!hasFocus) hideKeyboard(v) }
 
         viewModel.isFailedSignIn.observe(viewLifecycleOwner) {
             if (it.isNotEmpty()) {
                 binding.signInButton.isClickable = true
+                binding.demoButton.isClickable = true
                 binding.signInProgressBar.visibility = View.GONE
                 Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
             }
@@ -87,7 +84,7 @@ class SignInFragment : Fragment() {
         return binding.root
     }
 
-    fun hideKeyboard(v: View) {
+    private fun hideKeyboard(v: View) {
         val inputMethodManager = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(v.windowToken, 0)
     }
